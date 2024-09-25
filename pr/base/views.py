d3b3817078db3e52a5aaf090040596b2ad1c1864
ivose <ivoseeba@gmail.com>
2024-09-25 16:10:06 +0300

    crud rooms

--- pr/base/views.py
+++ pr/base/views.py
@@ -1,25 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 #from django.http import HttpResponse, HttpResponseNotFound
 #from . import views
 
-rooms = [
-    {'id':1, 'name':'Lets learn python'},
-    {'id':2, 'name':'Design with me'},
-    {'id':3, 'name':'Blaa-blaa-blaa'},
-]
-
 def home(request):
+    rooms = Room.objects.all()
     #return HttpResponse("Hello, world. You're at the pr index.")
     return render(request, 'base/home.html', {'rooms' : rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
+    room = Room.objects.get(id=pk)
     context = {'room' : room}
-    return render(request, 'room.html', context)
+    return render(request, 'base/room.html', context)
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            # request.POST.get('name')
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context = {'obj': room}
+    return render(request, 'base/delete.html', context)
 
 def custom_404_view(request, exception):
     return render(request, '404.html', status=404)
